d0da80f0e75d1f8505bed58ed47ee870e7aa8f85
PaulBrimley <paul.k.brimley@gmail.com>
2015-12-09 13:05:04 -0700

    completed all 4 assignments

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,11 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+
+String.prototype.reverse = function() {
+	return this.split('').reverse().join('');
+}
+
+var someString = 'This is a string';
+someString.reverse();
--- sayName.js
+++ sayName.js
@@ -1,13 +1,22 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
 //Now create three instances of Person with data you make up
 
   //code here
-
+var paul = new Person('Paul Brimley', 33);
+var jess = new Person('Jess Brimley', 33);
+var chuck = new Person('Chuck Noris', Infinity);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+
+Person.prototype.sayName = function() {
+	alert(this.name);
+}
